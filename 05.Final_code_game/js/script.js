e8d3d710766df59cc2418015e472b1b324aa7cd4
ykLiu <ken23912081@gmail.com>
2019-03-13 21:51:45 +0800

    upload 05.Final_code_game

--- 05.Final_code_game/js/script.js
+++ 05.Final_code_game/js/script.js
@@ -5,15 +5,22 @@ let b = 0; // b for bottom
 
 document.getElementById('guess_btn').onclick = function(){
     let ans = document.getElementById('answer').value;
-    if( ans > t || ans < b ){
+    if( parseInt(ans) > t || parseInt(ans) < b ){
         alert("Your answer is out of range !");
-    }else if( ans == final_code ){
+    }else if( parseInt(ans) == final_code ){
         alert("Congratulations ! You got the final code !")
-    }else if( ans < final_code){
+    }else if( parseInt(ans) < final_code){
         b = ans;
     }else{
         t = ans;
     }
-
+    document.getElementById('answer').value = "";
     document.getElementById('prompt').innerHTML = "請猜一個" + b + "到" + t + "之間的數";
 }
+
+document.getElementById('answer').addEventListener("keyup",function(event) {
+    event.preventDefault();
+    if(event.keyCode == 13){
+        document.getElementById('guess_btn').click();
+    }
+});
